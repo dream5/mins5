ce819b39cab18713a3c64f65c0bf372b94fefa3b
zt0454 <33329929@qq.com>
2014-03-08 19:01:09 -0800

    初始导入

---
+++ share/share-web/share-web-admin/src/main/java/com/mins5/share/web/controller/ArticleController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.mins5.share.web.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mins5.share.common.enums.GENDER;
+import com.mins5.share.web.controller.demo.TestAdmin;
+import com.mins5.share.web.controller.demo.TestUser;
+
+/**
+ * @author zhoutian
+ * @since 2014-3-9
+ */
+@Controller
+@Scope("prototype")
+@RequestMapping(value = "/article")
+public class ArticleController {
+
+	/**
+	 * 文章列表
+	 * @since 2014-3-9
+	 * @return
+	 */
+	@RequestMapping(value = "/list")
+	public ModelAndView list() {
+		System.out.println("list()");
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("article/article_list");
+		return mav;
+	} 
+}
