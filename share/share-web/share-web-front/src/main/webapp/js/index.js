6bfb2e2eee8f4f8e8f1a8ea9806ca56c160ff26e
zl0422 <286434733@qq.com>
2014-03-12 15:33:32 +0800

    主页处理ＪＳ

---
+++ share/share-web/share-web-front/src/main/webapp/js/index.js
@@ -0,0 +1,31 @@
+/**
+ *@author zhangl
+ *@since 20140312
+ */
+$(function (){
+	var timestamp = Date.parse(new Date());
+	$.ajax({
+	       type: "GET",
+	       contentType : 'application/json;charset=UTF-8',
+	       url: '/index/initNav.mins',
+	       data: {t:timestamp},
+	       dataType: "json",
+	       success:function(result) {
+		       if(result == "error"){
+		    	   $(".menu_nav").html("<ul><li><a href='http://www.mins5.com'>首页</a></li></ul>");
+		       }else{
+		    	   var navHtml = "<ul>";
+	    		   navHtml +="<li><a href='http://www.mins5.com'>首页</a></li>";
+		    	   $.each(result,function(index,item){
+		    		   navHtml +="<li><a href='#'>"+item.kindName+"</a></li>";
+		    	   });
+		    	   navHtml +="</ul>";
+		    	   $(".menu_nav").html(navHtml);
+		       } 
+	       },
+	       error:function(data){
+	    	   $(".menu_nav").html("<ul><li><a href='http://www.mins5.com'>首页</a></li></ul>");
+	       }
+	});
+	
+});
