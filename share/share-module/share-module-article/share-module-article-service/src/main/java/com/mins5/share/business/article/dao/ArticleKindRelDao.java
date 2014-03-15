fd6dcd6251c0be63f514ef9cc13d32c22006e0e2
zt0454 <33329929@qq.com>
2014-03-15 10:56:19 +0800

    添加文章类别关系表CRUD

---
+++ share/share-module/share-module-article/share-module-article-service/src/main/java/com/mins5/share/business/article/dao/ArticleKindRelDao.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.mins5.share.business.article.dao;
+
+import com.mins5.share.business.article.domain.ArticleKindRel;
+import com.mins5.share.common.dao.CrudDao;
+
+/**
+ * @author zhoutian
+ * @since 2014年3月15日
+ */
+public interface ArticleKindRelDao extends CrudDao<Long, ArticleKindRel> {
+
+}
