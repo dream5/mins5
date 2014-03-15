05c5b71617ac7fcbf3c6d19434c31b07b02d716b
zt0454 <33329929@qq.com>
2014-03-15 10:19:12 +0800

    添加文章标签关联表CRUD

---
+++ share/share-module/share-module-article/share-module-article-service/src/main/java/com/mins5/share/business/article/dao/ArticleLabelRelDao.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.mins5.share.business.article.dao;
+
+import com.mins5.share.business.article.domain.ArticleLabelRel;
+import com.mins5.share.common.dao.CrudDao;
+
+/**
+ * @author zhoutian
+ * @since 2014年3月15日
+ */
+public interface ArticleLabelRelDao extends CrudDao<Long, ArticleLabelRel> {
+
+}
