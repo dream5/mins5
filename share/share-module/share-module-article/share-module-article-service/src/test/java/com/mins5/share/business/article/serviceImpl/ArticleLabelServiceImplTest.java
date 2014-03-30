12b68d9efab53ca59cf47aab570b94e5866500af
zt0454 <33329929@qq.com>
2014-03-31 01:50:10 +0800

    添加文章类别和文章标签保存测试方法

--- share/share-module/share-module-article/share-module-article-service/src/test/java/com/mins5/share/business/article/serviceImpl/ArticleLabelServiceImplTest.java
+++ share/share-module/share-module-article/share-module-article-service/src/test/java/com/mins5/share/business/article/serviceImpl/ArticleLabelServiceImplTest.java
@@ -4,6 +4,7 @@
 package com.mins5.share.business.article.serviceImpl;
 
 import java.util.Date;
+import java.util.List;
 
 import junit.framework.Assert;
 
@@ -18,6 +19,7 @@ import com.mins5.share.business.article.domain.ArticleLabel;
 import com.mins5.share.business.article.service.ArticleLabelService;
 import com.mins5.share.common.service.ReturnCode;
 import com.mins5.share.common.service.ReturnData;
+import com.mins5.share.common.service.ReturnPageData;
 import com.mins5.share.common.service.VOID;
 
 /**
@@ -25,29 +27,33 @@ import com.mins5.share.common.service.VOID;
  * @since 2014年3月13日
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:applicationContext*.xml"})
+@ContextConfiguration(locations = { "classpath*:applicationContext*.xml" })
 @Transactional
 public class ArticleLabelServiceImplTest {
 
 	@Autowired
 	private ArticleLabelService articleLabelService;
-	
+
 	@Test
 	public void testSaveArticleLabel() {
 		ArticleLabel articleLabel = new ArticleLabel();
 		articleLabel.setCreateTime(new Date());
 		articleLabel.setLabelName("单元测试");
 		articleLabel.setStatus("1");
-		ReturnData<ArticleLabel> returnData = articleLabelService.saveArticleLabel(articleLabel);
-		Assert.assertEquals(ReturnCode.SUCCESS.getCode(), returnData.getReturnCode());
+		ReturnData<ArticleLabel> returnData = articleLabelService
+				.saveArticleLabel(articleLabel);
+		Assert.assertEquals(ReturnCode.SUCCESS.getCode(),
+				returnData.getReturnCode());
 	}
-	
+
 	@Test
 	public void testDeleteArticleLabelById() {
-		ReturnData<VOID> returnData = articleLabelService.deleteArticleLabelById(0L);
-		Assert.assertEquals(ReturnCode.SUCCESS.getCode(), returnData.getReturnCode());
+		ReturnData<VOID> returnData = articleLabelService
+				.deleteArticleLabelById(0L);
+		Assert.assertEquals(ReturnCode.SUCCESS.getCode(),
+				returnData.getReturnCode());
 	}
-	
+
 	@Test
 	public void testUpdateArticleLabel() {
 		ArticleLabel articleLabel = new ArticleLabel();
@@ -55,13 +61,32 @@ public class ArticleLabelServiceImplTest {
 		articleLabel.setCreateTime(new Date());
 		articleLabel.setLabelName("单元测试修改");
 		articleLabel.setStatus("0");
-		ReturnData<ArticleLabel> returnData = articleLabelService.updateArticleLabel(articleLabel);
-		Assert.assertEquals(ReturnCode.SUCCESS.getCode(), returnData.getReturnCode());
+		ReturnData<ArticleLabel> returnData = articleLabelService
+				.updateArticleLabel(articleLabel);
+		Assert.assertEquals(ReturnCode.SUCCESS.getCode(),
+				returnData.getReturnCode());
 	}
-	
+
 	@Test
 	public void testFindArticleLabelById() {
-		ReturnData<ArticleLabel> returnData = articleLabelService.findArticleLabelById(1L);
-		Assert.assertEquals(ReturnCode.SUCCESS.getCode(), returnData.getReturnCode());
+		ReturnData<ArticleLabel> returnData = articleLabelService
+				.findArticleLabelById(1L);
+		Assert.assertEquals(ReturnCode.SUCCESS.getCode(),
+				returnData.getReturnCode());
+	}
+
+	@Test
+	public void testFindArticleLabelByLabelNameAndStatusAndCreateTime() {
+		String labelName = "test";
+		String status = "1";
+		Date beginTime = new Date();
+		Date endTime = new Date();
+		int currentPage = 1;
+		int onePageSize = 10;
+		ReturnPageData<List<ArticleLabel>> returnData = articleLabelService
+				.findArticleLabelByLabelNameAndStatusAndCreateTime(labelName,
+						status, beginTime, endTime, currentPage, onePageSize);
+		Assert.assertEquals(ReturnCode.SUCCESS.getCode(),
+				returnData.getReturnCode());
 	}
 }
