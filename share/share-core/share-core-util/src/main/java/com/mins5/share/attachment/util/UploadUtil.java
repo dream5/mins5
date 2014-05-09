6a2cd3991ce1eb11b9bd4206a7a11fbea7d7d459
zl0422 <286434733@qq.com>
2014-05-09 16:27:13 +0800

    增加图片分割

--- share/share-core/share-core-util/src/main/java/com/mins5/share/attachment/util/UploadUtil.java
+++ share/share-core/share-core-util/src/main/java/com/mins5/share/attachment/util/UploadUtil.java
@@ -15,16 +15,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * <p>上传工具类</p>
+ * <p>
+ * 上传工具类
+ * </p>
+ * 
  * @author zhanglin
  * @since 20140430 17:15
  */
 public class UploadUtil {
 
-	private static final Log log  = LogFactory.getLog(UploadUtil.class);
-	
+	private static final Log log = LogFactory.getLog(UploadUtil.class);
+
 	/**
 	 * 单独文件上传操作
+	 * 
 	 * @param request
 	 * @param basePath 指定文件存放路径
 	 * @param serverPath 指定服务器存放文件路径
@@ -32,40 +36,40 @@ public class UploadUtil {
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static Map singleFileUpload(HttpServletRequest request) {
-		
+
 		log.info("单独文件上传开始...");
 		String fileType = request.getParameter("filetype");
-		if (fileType==null||fileType.trim().length()==0) {
+		if (fileType == null || fileType.trim().length() == 0) {
 			fileType = ".unknown";
-		}else{
+		} else {
 			fileType = fileType.toLowerCase();
 		}
-		String newFileName = String.valueOf(System.currentTimeMillis())+fileType;
+		String newFileName = String.valueOf(System.currentTimeMillis()) + fileType;
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		factory.setSizeThreshold(8192);
-		 //获取保存文件的最终路径
-		String saveFilePath = request.getSession().getServletContext().getRealPath("/")+"upload"+File.separator;
+		// 获取保存文件的最终路径
+		String saveFilePath = request.getSession().getServletContext().getRealPath("/") + "upload" + File.separator;
 		factory.setRepository(new File(saveFilePath));
 		ServletFileUpload upload = new ServletFileUpload(factory);
 		List uploadlist = null;
 		try {
 			uploadlist = upload.parseRequest(request);
 		} catch (FileUploadException e) {
-			log.error("上传文件异常：["+e.toString()+"]");
+			log.error("上传文件异常：[" + e.toString() + "]");
 			factory = null;
 			upload = null;
 			uploadlist = null;
-			 //如果是上传出现异常，删除文件
-			//FileUtil.del(saveFilePath);
+			// 如果是上传出现异常，删除文件
+			// FileUtil.del(saveFilePath);
 			return null;
 		}
 		String oldFileName = "", actualFileSize = "";
 		try {
 			for (int i = 0; i < uploadlist.size(); i++) {
 				FileItem item = (FileItem) uploadlist.get(i);
-				if (!item.isFormField()) { //处理文件上传域 忽略其他不是文件域的所有表单信息
+				if (!item.isFormField()) { // 处理文件上传域 忽略其他不是文件域的所有表单信息
 					try {
-						actualFileSize = String.valueOf(item.getSize()); //获取文件大小
+						actualFileSize = String.valueOf(item.getSize()); // 获取文件大小
 						item.write(new File(saveFilePath + newFileName));// 保存文件路径
 					} catch (Exception e) {
 						e.printStackTrace();
@@ -85,6 +89,13 @@ public class UploadUtil {
 			uploadlist = null;
 		}
 
+		String fullName = saveFilePath + newFileName;
+		log.info("生成文件的完整路径：" + fullName);
+		// 生成不同尺寸的图片
+		ImageUtils.scale2(fullName, saveFilePath + "_730x260.png", 260, 730, true);// 测试OK
+		ImageUtils.scale2(fullName, saveFilePath + "_613x193.png", 193, 613, true);
+		ImageUtils.scale2(fullName, saveFilePath + "_212x212.png", 212, 212, true);
+
 		/*
 		 * String[] rtnVal = new String[5]; rtnVal[0] = oldFileName; 旧文件名
 		 * rtnVal[1] = fileType; 带点的扩展名，如 .wmv rtnVal[2] = newFileName;
@@ -93,12 +104,12 @@ public class UploadUtil {
 		 */
 		Map rtnMap = new HashMap();
 		rtnMap.put("newName", newFileName);
-		//rtnMap.put("oldName", oldFileName);
+		// rtnMap.put("oldName", oldFileName);
 		return rtnMap;
 	}
-	
-	public static void main(String[]args){
+
+	public static void main(String[] args) {
 		System.out.println();
 	}
-	
+
 }
