156a6897188c402b8259476bd45fbd016919675f
dreamfiveCloud <cry615@sina.com>
2014-05-24 12:03:22 +0800

    Admin -chenry

--- share/share-module/share-module-admin/share-module-admin-service/src/main/java/com/mins5/share/business/admin/dao/AdminDao.java
+++ share/share-module/share-module-admin/share-module-admin-service/src/main/java/com/mins5/share/business/admin/dao/AdminDao.java
@@ -1,5 +1,8 @@
 package com.mins5.share.business.admin.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import com.mins5.share.business.admin.domain.Admin;
@@ -12,4 +15,29 @@ import com.mins5.share.common.dao.CrudDao;
 @Service
 public interface AdminDao extends CrudDao<Long, Admin> {
 
+	
+	Admin findByUserNameAndPassword(String username ,String password);
+	
+	
+	/**
+	 * 根据Admin对象查询
+	 * 
+	 * @author chenry
+	 * @since 2014年3月13日
+	 * @param adminId
+	 * @return
+	 */
+	Long findAdminCountByModel(Admin admin);
+	
+	/**
+	 * 分页查询管理员列表
+	 * @author chenry
+	 * @since 2014年3月22日
+	 * @param admin 查询参数
+	 * @param currentPage 当前页
+	 * @param onePageSize 每行行数
+	 * @return
+	 */
+	List<Admin> findAdminListByModel(Admin admin,
+			@Param("startRow") int startRow, @Param("onePageSize") int onePageSize);
 }
