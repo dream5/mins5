422d6e2c266c1242ea496330ac8600a405cbeeac
dreamfiveCloud <cry615@sina.com>
2014-05-24 12:04:46 +0800

    Admin -chenry

--- share/share-module/share-module-admin/share-module-admin-service/src/main/java/com/mins5/share/business/admin/serviceImpl/AdminServiceImpl.java
+++ share/share-module/share-module-admin/share-module-admin-service/src/main/java/com/mins5/share/business/admin/serviceImpl/AdminServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mins5.share.business.admin.serviceImpl;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -7,17 +8,20 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import com.mins5.share.business.admin.dao.AdminDao;
 import com.mins5.share.business.admin.domain.Admin;
 import com.mins5.share.business.admin.service.AdminService;
 import com.mins5.share.common.service.ReturnCode;
 import com.mins5.share.common.service.ReturnData;
+import com.mins5.share.common.service.ReturnPageData;
 import com.mins5.share.common.service.VOID;
 
 /**
- * @author zhoutian
- * @since 2014-2-27
+ * 
+ * @author chenry
+ * @since 2014年5月23日
  */
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -110,7 +114,9 @@ public class AdminServiceImpl implements AdminService {
 				returnData.setReturnCode(ReturnCode.PARAM_ERROR.getCode());
 				return returnData;
 			}
+			System.out.println(AdminId  + "---------------");
 			Admin Admin = adminDao.findById(AdminId);
+			System.out.println(Admin  + "---------------");
 			returnData.setResultData(Admin);
 			returnData.setReturnCode(ReturnCode.SUCCESS.getCode());
 		} catch (Exception e) {
@@ -120,18 +126,29 @@ public class AdminServiceImpl implements AdminService {
 		return returnData;
 	}
 
-	/*@Override
-	public ReturnPageData<List<Admin>> findAdminListByCondition(Map params, int currentPage, int onePageSize) {
+	@Override
+	public Long findAdminCountByModel(Admin admin) {
+		return adminDao.findAdminCountByModel(admin);
+	}
+
+	@Override
+	public ReturnPageData<List<Admin>> findAdminListByModel(Admin admin, int currentPage, int onePageSize) {
+		
 		ReturnPageData<List<Admin>> returnData = new ReturnPageData<List<Admin>>(currentPage, onePageSize);
 		try {
-			int count = adminDao.findAdminCount(params);
+			long count = adminDao.findAdminCountByModel(admin);
 			if (count > 0) {
 				int startRow = returnData.getStartRow();
-				params.put("startRow", startRow);
-				params.put("onePageSize", onePageSize);
-				List<Admin> AdminList = adminDao.findAdminList(params);
+				System.out.println(admin+ "......");
+				System.out.println(admin.getUserName()+ "......");
+
+				List<Admin> adminList = adminDao.findAdminListByModel(admin, startRow, onePageSize);
+				System.out.println(adminList+ "......");
 				returnData.setTotalResults(count);
-				returnData.setResultData(AdminList);
+				if (StringUtils.isEmpty(adminList)) {
+					adminList = Collections.emptyList();
+				}
+				returnData.setResultData(adminList);
 			}
 			returnData.setReturnCode(ReturnCode.SUCCESS.getCode());
 		} catch (Exception e) {
@@ -139,11 +156,8 @@ public class AdminServiceImpl implements AdminService {
 			returnData.setReturnCode(ReturnCode.EXCEPTION.getCode());
 		}
 		return returnData;
-	}*/
-
-	@Override
-	public ReturnData<List<Admin>> findAdminByNum(int amount) {
-		return null;
+		
 	}
+
 	
 }
