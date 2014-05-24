11073ca0f73311ba08aba9f8b2753db88774e8ed
dreamfiveCloud <cry615@sina.com>
2014-05-24 12:04:30 +0800

    Admin -chenry

--- share/share-module/share-module-admin/share-module-admin-api/src/main/java/com/mins5/share/business/admin/service/AdminService.java
+++ share/share-module/share-module-admin/share-module-admin-api/src/main/java/com/mins5/share/business/admin/service/AdminService.java
@@ -1,14 +1,17 @@
 package com.mins5.share.business.admin.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.mins5.share.business.admin.domain.Admin;
 import com.mins5.share.common.service.ReturnData;
+import com.mins5.share.common.service.ReturnPageData;
 import com.mins5.share.common.service.VOID;
 
 /**
- * @author zhoutian
- * @since 2014-2-27
+ * 
+ * @author chenry
+ * @since 2014年5月23日
  */
 public interface AdminService {
 
@@ -16,7 +19,7 @@ public interface AdminService {
 	/**
 	 * 新增
 	 * 
-	 * @author zhoutian
+	 * @author chenry
 	 * @since 2014年3月13日
 	 * @param Admin
 	 * @return
@@ -26,17 +29,17 @@ public interface AdminService {
 	/**
 	 * 根据ID删除
 	 * 
-	 * @author zhoutian
+	 * @author chenry
 	 * @since 2014年3月13日
-	 * @param AdminId
+	 * @param adminId
 	 * @return
 	 */
-	ReturnData<VOID> deleteAdminById(Long labelId);
+	ReturnData<VOID> deleteAdminById(Long adminId);
 
 	/**
 	 * 更新
 	 * 
-	 * @author zhoutian
+	 * @author chenry
 	 * @since 2014年3月13日
 	 * @param Admin
 	 * @return
@@ -46,20 +49,33 @@ public interface AdminService {
 	/**
 	 * 根据ID查询
 	 * 
-	 * @author zhoutian
+	 * @author chenry
 	 * @since 2014年3月13日
-	 * @param labelId
+	 * @param adminId
 	 * @return
 	 */
-	ReturnData<Admin> findAdminById(Long labelId);
-
+	ReturnData<Admin> findAdminById(Long adminId);
+	
 	/**
-	 * 查询一定数目的标签
+	 * 根据Admin对象查询
 	 * 
-	 * @param amount
-	 *            数量
+	 * @author chenry
+	 * @since 2014年3月13日
+	 * @param adminId
 	 * @return
 	 */
-	ReturnData<List<Admin>> findAdminByNum(int amount);
+	Long findAdminCountByModel(Admin admin);
+	
+	/**
+	 * 分页查询管理员列表
+	 * @author chenry
+	 * @since 2014年3月22日
+	 * @param admin 查询参数
+	 * @param currentPage 当前页
+	 * @param onePageSize 每行行数
+	 * @return
+	 */
+	ReturnPageData<List<Admin>> findAdminListByModel(Admin admin,
+			int currentPage, int onePageSize);
 
 }
