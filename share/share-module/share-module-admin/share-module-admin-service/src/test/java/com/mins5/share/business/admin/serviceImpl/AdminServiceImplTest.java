cf77079a49a45618dc5034230eb5db8660116ef1
dreamfiveCloud <cry615@sina.com>
2014-05-24 11:59:45 +0800

    Admin Test

--- share/share-module/share-module-admin/share-module-admin-service/src/test/java/com/mins5/share/business/admin/serviceImpl/AdminServiceImplTest.java
+++ share/share-module/share-module-admin/share-module-admin-service/src/test/java/com/mins5/share/business/admin/serviceImpl/AdminServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.mins5.share.business.admin.serviceImpl;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +13,7 @@ import com.mins5.share.business.admin.domain.Admin;
 import com.mins5.share.business.admin.service.AdminService;
 import com.mins5.share.common.service.ReturnCode;
 import com.mins5.share.common.service.ReturnData;
+import com.mins5.share.common.service.ReturnPageData;
 
 /**
  * @author zhoutian
@@ -27,6 +30,22 @@ public class AdminServiceImplTest {
 	public void testFindAdminById() {
 		Long adminId = 1L;
 		ReturnData<Admin> returnData = adminService.findAdminById(adminId);
+		Assert.assertEquals(ReturnCode.SUCCESS.getCode(), returnData.getReturnCode());
+	}
+	
+	@Test
+	public void testFindAdminCountByModel() {
+		Admin admin = new Admin();
+		admin.setUserName("天");
+		long returnData = adminService.findAdminCountByModel(admin);
+		Assert.assertNull("", returnData);
+	}
+	
+	@Test
+	public void testFindAdminListByModel() {
+		Admin admin = new Admin();
+		admin.setUserName("c");
+		ReturnPageData<List<Admin>> returnData = adminService.findAdminListByModel(admin,0,10);
 		Assert.assertEquals(ReturnCode.SUCCESS.getCode(), returnData.getReturnCode());
 	}
 }
