604ecb7723163a821e521c1a37556e482cd77583
dreamfiveCloud <cry615@sina.com>
2014-05-27 10:39:34 +0800

    StringUtils修改成MyStringUtils -chenry

--- share/share-core/share-core-util/src/test/java/com/mins5/util/StringUtilsTest.java
+++ share/share-core/share-core-util/src/test/java/com/mins5/util/StringUtilsTest.java
@@ -4,7 +4,7 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
-import com.mins5.share.util.StringUtils;
+import com.mins5.share.util.MyStringUtils;
 
 /**
  * @author mins5
@@ -13,78 +13,78 @@ import com.mins5.share.util.StringUtils;
 public class StringUtilsTest {
 
 	@Test
-    public void testIsBlank() {
-		String str= null;
-		boolean isBlank = StringUtils.isBlank(str);
+	public void testIsBlank() {
+		String str = null;
+		boolean isBlank = MyStringUtils.isBlank(str);
 		Assert.assertTrue(isBlank);
-		
+
 		str = "";
-		isBlank = StringUtils.isBlank(str);
+		isBlank = MyStringUtils.isBlank(str);
 		Assert.assertTrue(isBlank);
-		
+
 		str = "  ";
-		isBlank = StringUtils.isBlank(str);
+		isBlank = MyStringUtils.isBlank(str);
 		Assert.assertTrue(isBlank);
-		
+
 		str = "  abc  ";
-		isBlank = StringUtils.isBlank(str);
+		isBlank = MyStringUtils.isBlank(str);
 		Assert.assertFalse(isBlank);
-    }
-	
-    @Test
-    public void testTrimBlank() {
-    	String str= null;
-		String actual = StringUtils.trimBlank(str);
+	}
+
+	@Test
+	public void testTrimBlank() {
+		String str = null;
+		String actual = MyStringUtils.trimBlank(str);
 		Assert.assertNull(actual);
-		
+
 		str = "";
-		actual = StringUtils.trimBlank(str);
+		actual = MyStringUtils.trimBlank(str);
 		Assert.assertEquals("", actual);
-		
+
 		str = "  ";
-		actual = StringUtils.trimBlank(str);
+		actual = MyStringUtils.trimBlank(str);
 		Assert.assertEquals("", actual);
-		
+
 		str = "  abc  ";
-		actual = StringUtils.trimBlank(str);
-		Assert.assertEquals("abc", actual);
-    }
-    
-    @Test
-	public void testParseNull() {
-    	String str= null;
-		String actual = StringUtils.parseNull(str);
-		Assert.assertNull(actual);
-		
-		str = "";
-		actual = StringUtils.parseNull(str);
-		Assert.assertNull(actual);
-		
-		str = "  ";
-		actual = StringUtils.parseNull(str);
-		Assert.assertNull(actual);
-		
-		str = "  abc  ";
-		actual = StringUtils.parseNull(str);
+		actual = MyStringUtils.trimBlank(str);
 		Assert.assertEquals("abc", actual);
 	}
-	
-    @Test
-	public void testParseEmpty() {
-    	String str= null;
-		String actual = StringUtils.parseEmpty(str);
-		Assert.assertEquals("", actual);
-		
+
+	@Test
+	public void testParseNull() {
+		String str = null;
+		String actual = MyStringUtils.parseNull(str);
+		Assert.assertNull(actual);
+
 		str = "";
-		actual = StringUtils.parseEmpty(str);
-		Assert.assertEquals("", actual);
-		
+		actual = MyStringUtils.parseNull(str);
+		Assert.assertNull(actual);
+
 		str = "  ";
-		actual = StringUtils.parseEmpty(str);
-		Assert.assertEquals("", actual);
-		
+		actual = MyStringUtils.parseNull(str);
+		Assert.assertNull(actual);
+
 		str = "  abc  ";
-		actual = StringUtils.parseEmpty(str);
+		actual = MyStringUtils.parseNull(str);
+		Assert.assertEquals("abc", actual);
+	}
+
+	@Test
+	public void testParseEmpty() {
+		String str = null;
+		String actual = MyStringUtils.parseEmpty(str);
+		Assert.assertEquals("", actual);
+
+		str = "";
+		actual = MyStringUtils.parseEmpty(str);
+		Assert.assertEquals("", actual);
+
+		str = "  ";
+		actual = MyStringUtils.parseEmpty(str);
+		Assert.assertEquals("", actual);
+
+		str = "  abc  ";
+		actual = MyStringUtils.parseEmpty(str);
 		Assert.assertEquals("abc", actual);
 	}
 }
