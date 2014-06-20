cab366f4214f4ad197d4b83f72baca1a808f724e
zl0422 <286434733@qq.com>
2014-06-20 15:53:06 +0800

    删除无用代码

--- share/share-web/share-web-front/src/main/java/com/mins5/share/web/controller/index/IndexController.java
+++ share/share-web/share-web-front/src/main/java/com/mins5/share/web/controller/index/IndexController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONArray;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Scope;
@@ -15,12 +13,10 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mins5.share.business.article.domain.Article;
-import com.mins5.share.business.article.domain.ArticleKind;
 import com.mins5.share.business.article.domain.ArticleLabel;
 import com.mins5.share.business.article.enums.RECOMMEND_POSITION;
 import com.mins5.share.common.service.ReturnData;
 import com.mins5.share.common.service.ReturnPageData;
-import com.mins5.share.util.JsonUtils;
 import com.mins5.share.web.controller.base.BaseController;
 
 /**
@@ -86,92 +82,6 @@ public class IndexController extends BaseController {
 		}
 
 		return "index";
-	}
-
-	/**
-	 * 获取分页文章
-	 * 
-	 * @param request
-	 * @param response
-	 */
-	@RequestMapping(value = "/getArticlesByPageNo")
-	@Deprecated
-	public void getArticlesByPageNo(HttpServletRequest request, HttpServletResponse response) {
-		int currentPage = 1;
-		int pageSize = 10;
-		if (!StringUtils.isEmpty(request.getParameter("cp"))) {
-			currentPage = Integer.parseInt(request.getParameter("cp"));
-		}
-		if (!StringUtils.isEmpty(request.getParameter("ps"))) {
-			pageSize = Integer.parseInt(request.getParameter("ps"));
-			if (pageSize > 10) {// 防止恶意传参
-				JsonUtils.write("error", response);
-				return;
-			}
-		}
-		ReturnPageData<List<Article>> returnData = articleService.findArticleByCondition(null, currentPage, pageSize);
-		if (returnData.getReturnCode() == 200 && !StringUtils.isEmpty(returnData.getResultData())) {
-			JsonUtils.write(returnData.getResultData(), response);
-		} else {
-			JsonUtils.write("error", response);
-		}
-	}
-
-	/**
-	 * 初始化导航菜单 2014 0417迁移到父类中
-	 */
-	@RequestMapping(value = "/initNav")
-	@Deprecated
-	public void initNavigate(HttpServletResponse response) {
-		log.info("加载主页导航菜单...");
-		ReturnData<List<ArticleKind>> articleKinds = articleService.findAllArticleKind();
-		if (articleKinds.getReturnCode() == 200 && !StringUtils.isEmpty(articleKinds.getResultData())) {
-			JSONArray articleKind = JSONArray.fromObject(articleKinds.getResultData());
-			JsonUtils.write(articleKind.toString(), response);
-		} else {
-			JsonUtils.write("error", response);
-		}
-	}
-
-	/**
-	 * 随机阅读
-	 */
-	@RequestMapping(value = "/randomRead")
-	@Deprecated
-	public void randomRead(HttpServletRequest request, HttpServletResponse response) {
-		log.info("加载随机阅读文章...");
-		int amount = 8;// 默认取八篇
-		if (!StringUtils.isEmpty(request.getParameter("a"))) {
-			amount = Integer.parseInt(request.getParameter("a"));
-		}
-		ReturnData<List<Article>> articles = articleService.findRandomArticle(amount);
-		if (articles.getReturnCode() == 200 && !StringUtils.isEmpty(articles.getResultData())) {
-			JSONArray article = JSONArray.fromObject(articles.getResultData());
-			JsonUtils.write(article.toString(), response);
-		} else {
-			JsonUtils.write("error", response);
-		}
-	}
-
-	/**
-	 * 热门标签
-	 */
-	@RequestMapping(value = "/hotLabel")
-	@Deprecated
-	public void hotLabel(HttpServletRequest request, HttpServletResponse response) {
-		log.info("加载热门标签...");
-		int amount = 8;// 默认取八篇
-		if (!StringUtils.isEmpty(request.getParameter("a"))) {
-			amount = Integer.parseInt(request.getParameter("a"));
-		}
-		ReturnData<List<ArticleLabel>> articleLabels = articleLabelService.findArticleLabelByNum(amount);
-		if (articleLabels.getReturnCode() == 200 && !StringUtils.isEmpty(articleLabels.getResultData())) {
-			JSONArray articleLabel = JSONArray.fromObject(articleLabels.getResultData());
-			JsonUtils.write(articleLabel.toString(), response);
-		} else {
-			JsonUtils.write("error", response);
-		}
-
 	}
 
 }
